5de64b03bec62f379ebc295858874f528d202e77
Tasneemmha <tasneemmahmuod2@gmail.com>
2023-01-22 20:33:36 +0300

    update

---
+++ app.js
@@ -0,0 +1,19 @@
+let tasneem_information = [
+    myPersonalInformation = {
+        "name": "taneem",  //string
+        "age": 29, //integer
+        "study": "MIS", //string
+        "familary_members": ["father , mother , Sister , brother"],
+
+        "courses": {
+            "course_one": "Automation ",
+            "course_two": "manual",
+        }  //object
+    },
+
+    myLocationInformation = {
+        "city": "amman",
+        "country": "jordan",
+        "work_place": "jordan"
+    }
+]
